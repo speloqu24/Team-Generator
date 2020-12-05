c59a83fdc85f9d0d07795309a81a28aa6d337f19
Speloquin <speloqu24@gmail.com>
2020-12-04 17:49:23 -0700

    Final run. Ran an npm test 4/4 17/17 ran. Ran application. Developed README

--- Develop/app.js
+++ Develop/app.js
@@ -12,8 +12,9 @@ const render = require("./lib/htmlRenderer");
 // const Employee = require("./lib/Employee");
 
 const teamArray = [];
+
 // ---------------------------------------------------
-// Initial Manager Questions, creating the Manager
+// Create Manager profile
 // ---------------------------------------------------
 
 const createManager = () => {
@@ -52,6 +53,10 @@ const createManager = () => {
       addAnother();
     });
 };
+
+// ---------------------------------------------------
+// Confirming another teamMember should be added
+// ---------------------------------------------------
 
 const addAnother = () => {
   inquirer
@@ -99,7 +104,7 @@ const addTeamMember = () => {
 };
 
 // ---------------------------------------------------
-// Adding Intern
+// Create Intern Profile
 // ---------------------------------------------------
 
 const createIntern = () => {
@@ -140,7 +145,7 @@ const createIntern = () => {
 };
 
 // ---------------------------------------------------
-// Adding Engineer
+// Create Engineer Profile
 // ---------------------------------------------------
 
 const createEngineer = () => {
@@ -181,21 +186,3 @@ const createEngineer = () => {
 };
 
 createManager();
-
-// // const teamArray = [];
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
--- README.md
+++ README.md
@@ -1,17 +1,40 @@
-# Team-Generator
+# ![License](https://img.shields.io/badge/license-NONE-green.svg) Title: Team Generator
 
-// const manager = new Manager(this.name);
-// managerArray.push(manager);
+Samantha Peloquin
 
-// const engineer = new Engineer("sam", 12345, "me@me.com", "speloqu24");
-// // response.name
-// teamArray.push(engineer);
+> Deployed Application:
 
-// const renString = render(teamArray, managerArray);
-// fs.writeFile(outputPath, renString, (err) => {
-// if (err) throw err;
-// // console.log("success");
-// });
+## Table of Contents
 
-// console.log(renString);
-// console.log(engineer);
+- [Project Description](#description)
+- [Installation Details](#installation)
+- [Usage](#usage)
+- [Questions](#questions)
+
+## Description
+
+---
+
+> _This application was created to be used internally by managers to develop a team of engineers and interns._
+
+## Installation
+
+---
+
+> Instructions: npm i, run the test if desired by entering npm test in the command line.
+>
+> _To begin install please head on over to the repo found in the [Questions](#questions) section of this README_
+
+## Usage
+
+---
+
+> This project can be used for quick team/roster creation resulting in a professional developed front end display.
+
+## Questions
+
+---
+
+> _If you have any questions at all please don't hesitate to review the repo or email me directly_
+>
+> [![Generic badge](https://img.shields.io/badge/Github-dodgerblue.svg)](https://github.com/speloqu24) > [![Generic badge](https://img.shields.io/badge/Email-dodgerblue.svg)](mailto:speloqu24@gmail.com)
